fbb1622e834986ce00dfbaf80de698802de6240a
sairuk <sairuk@gmail.com>
2020-02-25 17:10:29 +1100

    Hack in python3 support

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
 # gPodder_pc_Import - Import existing Podcast episodes into gPodder
@@ -18,6 +18,7 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 import os
+import sys
 import sqlite3
 #
 ###############################################################################
@@ -36,52 +37,57 @@ match_types = ['.m4a','.m4v','.mp3','.mp4','.ogg','.ogv']
 
 def main():
     # Open Database
-    conn = sqlite3.connect(db) or die ("Couldn't open %s" % db)
-    # Read Podcast URLS from database
-    pc_result = conn.execute("SELECT id, url FROM episode WHERE url IS NOT NULL")
+    if os.path.exists(db):
+        conn = sqlite3.connect(db) or die ("Couldn't open %s" % db)
+        # Read Podcast URLS from database
+        try:
+            pc_result = conn.execute("SELECT id, url FROM episode WHERE url IS NOT NULL")
+        except sqlite3.OperationalError:
+            print("Invalid db file: %s" % db)
+            sys.exit()
 
-    # Build a modified listing from the sql data
-    pc_url = []
-    for row in pc_result:
-        filename, ext = os.path.splitext(row[1].split('/')[-1])
-        # Remove URL args when a podcast returns
-        if '?' in ext:
-            ext = ext.split('?')[0]
-        pc_url.append([row[0],'%s%s' % (filename.replace('.','_'),ext)])
+        # Build a modified listing from the sql data
+        pc_url = []
+        for row in pc_result:
+            filename, ext = os.path.splitext(row[1].split('/')[-1])
+            # Remove URL args when a podcast returns
+            if '?' in ext:
+                ext = ext.split('?')[0]
+            pc_url.append([row[0], '%s%s' % (filename.replace('.', '_'), ext)])
 
-    # Build list of available files in dl_loc
-    pc_loc = []
-    for root, dirs, files in os.walk(dl_loc, 'topdown=FALSE'):
-        for fname in files:
-            filename, ext = os.path.splitext(fname)          
-            if ext in match_types:
-                pc_loc.append(['%s%s' % (fname[:fname.rfind('_')],ext),fname])
-            else:
-                pass # Do Nothing
-                
-
-    # Match file types, update database information
-    cntr = 0
-    for row2, fname in pc_loc:
-        for id, x in pc_url:
-            if row2 in x:
-                cntr += 1
-                try:
-                    print "FOUND: %s, updating gpodder database" % (row2)
-                    conn.execute("UPDATE episode SET state = %d, is_new = %d, download_filename = '%s' WHERE id = '%d'" % (state,is_new,fname,id))
-                    if sql:
-                        conn.commit()
-                except:
+        # Build list of available files in dl_loc
+        pc_loc = []
+        for root, dirs, files in os.walk(dl_loc, 'topdown=FALSE'):
+            for fname in files:
+                filename, ext = os.path.splitext(fname)
+                if ext in match_types:
+                    pc_loc.append(['%s%s' % (fname[:fname.rfind('_')], ext), fname])
+                else:
                     pass # Do Nothing
-            else:
-                pass # Do Nothing
-                
-    # Print results to screen
-    print "OLD EPISODES FOUND: %d" % len(pc_loc)
-    print "OLD EPISODES MATCHED: %d" % cntr
-    
-    # Close Database
-    conn.close() 
-    
+
+        # Match file types, update database information
+        cntr = 0
+        for row2, fname in pc_loc:
+            for id, x in pc_url:
+                if row2 in x:
+                    cntr += 1
+                    try:
+                        print("FOUND: %s, updating gpodder database" % (row2))
+                        conn.execute("UPDATE episode SET state = %d, is_new = %d, download_filename = '%s' WHERE id = '%d'" % (state, is_new, fname, id))
+                        if sql:
+                            conn.commit()
+                    except sqlite3.OperationalError:
+                        pass # Do Nothing
+                else:
+                    pass # Do Nothing
+
+        # Print results to screen
+        print("OLD EPISODES FOUND: %d" % len(pc_loc))
+        print("OLD EPISODES MATCHED: %d" % cntr)
+
+        # Close Database
+        conn.close()
+    else:
+        print("Could not find database file: %s" % db)
 if __name__ == "__main__":
     main()
